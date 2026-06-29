09e137075bb65b6c832399d98f5af354171647c7
Ben Knight <benknight135@gmail.com>
2026-06-29 16:33:01 +0100

    feat(expose): expose pkg

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      packages.${system}.audiorelay =
+        (pkgs.callPackage ./default.nix { }).audiorelay;
+
       packages.${system}.default =
-        pkgs.callPackage ./default.nix { };
+        (pkgs.callPackage ./default.nix { }).audiorelay;
     };
 }
