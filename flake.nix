29f91aa392bd691496c3ba6b578f53e47e0da9b9
Ben Knight <benknight135@gmail.com>
2026-06-29 18:31:20 +0100

    fix(import): remove nix import

--- flake.nix
+++ flake.nix
@@ -7,18 +7,11 @@
 
   outputs = { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" ];
+      system = "x86_64-linux";
     in {
-      packages = nixpkgs.lib.genAttrs systems (system:
-        let 
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-        in {
-          audiorelay = pkgs.callPackage ./default.nix { };
-          default = self.packages.${system}.audiorelay;
-        }
-      );
+      packages.${system} = { pkgs, ... }:
+        pkgs.callPackage ./default.nix {};
+
+      defaultPackage.${system} = self.packages.${system};
     };
 }
